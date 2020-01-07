1b7bcf5031e36cef0d98f78c56e409c57b237c91
aibou <aibou7251@gmail.com>
2020-01-07 18:36:59 +0900

    Update Cmd, Option

--- keyboards/ergo42/keymaps/aibou/keymap.c
+++ keyboards/ergo42/keymaps/aibou/keymap.c
@@ -12,21 +12,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* BASE
    * ,------------------------------------------------.   ,------------------------------------------------.
-   * | Tab  |   Q  |   W  |   E  |   R  |   T  |  [   |   |  ]   |   Y  |   U  |   I  |   O  |   P  |  @   |
+   * |   =  |   Q  |   W  |   E  |   R  |   T  | ESC  |   | TAB  |   Y  |   U  |   I  |   O  |   P  |  -   |
    * |------+------+------+------+------+------+------|   |-------------+------+------+------+------+------|
-   * | Alt  |   A  |   S  |   D  |   F  |   G  |  (   |   |  )   |   H  |   J  |   K  |   L  |   ;  |  :   |
+   * | Ctrl |   A  |   S  |   D  |   F  |   G  |      |   |      |   H  |   J  |   K  |   L  |   ;  |  :   |
    * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
-   * | Sft  |   Z  |   X  |   C  |   V  |   B  |  {   |   |  }   |   N  |   M  |   ,  |   .  |   /  |\/Sft |
+   * | Sft  |   Z  |   X  |   C  |   V  |   B  |      |   |      |   N  |   M  |   ,  |   .  |   /  | Sft  |
    * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
-   * | Ctrl | GUI  |  App |PrtSc |ESC/  |Space/|Tab/  |   |Back  |Enter/| Del  |PrtSc |=>GAME|=>SYMB|  \   |
-   * |      |      |      |      |~SYMB |Cmd   |Shift |   |Space |~META |      |      |      |      |      |
+   * | META |   \  |   `  |Option| Cmd  | DEL  |Space |   |Enter |BackSp|  Cmd |Option|   [  |   ]  | META |
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [BASE] = LAYOUT( \
     KC_EQL,   KC_Q,    KC_W,   KC_E,    KC_R,    KC_T,   KC_ESC,     KC_TAB,  KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_MINS, \
-    KC_LCTRL, KC_A,    KC_S,   KC_D,    KC_F,    KC_G,   KC_LGUI,    KC_LGUI, KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT, \
-    KC_LSFT,  KC_Z,    KC_X,   KC_C,    KC_V,    KC_B,   KC_LALT,    KC_LALT, KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT, \
-    MO(META), KC_BSLS, KC_GRV, XXXXXXX, XXXXXXX, KC_DEL, KC_SPC,     KC_ENT,  KC_BSPC, XXXXXXX, XXXXXXX,  KC_LBRC,  KC_RBRC,  MO(META) \
+    KC_LCTRL, KC_A,    KC_S,   KC_D,    KC_F,    KC_G,   _______,    _______, KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT, \
+    KC_LSFT,  KC_Z,    KC_X,   KC_C,    KC_V,    KC_B,   _______,    _______, KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT, \
+    MO(META), KC_BSLS, KC_GRV, KC_LALT, KC_LGUI, KC_DEL, KC_SPC,     KC_ENT,  KC_BSPC, KC_RGUI, KC_RALT,  KC_LBRC,  KC_RBRC,  MO(META) \
   ),
 
   /* META
